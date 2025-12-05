ea6306e09c05935226041ab069465bdf228fcf82
Harsh Singh <harsh_singh@srmap.edu.in>
2025-12-05 21:49:16 +0530

    deployment chnages

--- research-backend/src/utils/tokens.ts
+++ research-backend/src/utils/tokens.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import jwt, { SignOptions } from "jsonwebtoken";
 import { env } from "../config/env";
 
 export interface JwtPayload {
@@ -8,11 +8,15 @@ export interface JwtPayload {
   exp?: number;
 }
 
-export const signAccessToken = (payload: JwtPayload) =>
-  jwt.sign(payload, env.jwt.accessSecret, { expiresIn: env.jwt.accessExpires });
+export const signAccessToken = (payload: JwtPayload): string => {
+  const options: SignOptions = { expiresIn: env.jwt.accessExpires };
+  return jwt.sign(payload, env.jwt.accessSecret, options);
+};
 
-export const signRefreshToken = (payload: JwtPayload) =>
-  jwt.sign(payload, env.jwt.refreshSecret, { expiresIn: env.jwt.refreshExpires });
+export const signRefreshToken = (payload: JwtPayload): string => {
+  const options: SignOptions = { expiresIn: env.jwt.refreshExpires };
+  return jwt.sign(payload, env.jwt.refreshSecret, options);
+};
 
 export const verifyAccessToken = (token: string) =>
   jwt.verify(token, env.jwt.accessSecret) as JwtPayload;
